732aaaf02a8847b72fcb0fa0fa3b6728d5224f22
Hanhsokiu <165264893+Hanhsokiu@users.noreply.github.com>
2024-11-02 00:03:58 +0700

    Delete BAITAPLON directory

--- BAITAPLON/CONTACT/quanly.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.querySelector('.menu');
-const mainNav = document.querySelector('.main-nav')
-
-menuToggle.addEventListener('click', () => {
-    mainNav.classList.toggle('menuclick');
-});
